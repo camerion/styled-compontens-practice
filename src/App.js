f47033c2f016811964c81cee982d683e1955cd21
camerion <>
2021-03-24 18:15:53 -0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import styled, {css, ThemeProvider} from 'styled-components'
+import { createRef } from 'react';
 
+const LearnReact = styled.a`
+  color: white;
+  font-size: 36px;
+  color: ${props => props.color ? props.color : "white;"}
+  ${props => props.primary && css`
+  font-size: 48px;
+  color: yellow;
+  transition: ease 0.25s all;
+  `};
+  :hover {
+    font-size: 48px;
+    opacity: 0.5;
+  }
+`
+
+const LearnMoreReact = styled ( LearnReact)`
+font-size: 18px;
+color:red;
+`
+const theme = {
+  primary: {
+    baseColor: "#888"
+  }
+}
+
+const Body = styled.div`
+text-align: center;
+`
+;
 function App() {
+  const [primary, setPrimary] = useState(false)
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+    <Body>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <LearnMoreReact>testing</LearnMoreReact>
+        <LearnReact primary = {primary} onClick = {
+          () => setPrimary (!primary)}>
           Learn React
-        </a>
+        </LearnReact>
       </header>
-    </div>
+    </Body>
+    </ThemeProvider>
   );
 }
 
